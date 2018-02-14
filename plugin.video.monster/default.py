f3e49b8d8ba587a65d741b0a93233a8c7391a7c4
David <bluedangerforyou@gmail.com>
2018-02-14 08:47:52 -0800

    updated
    updated

--- plugin.video.monster/default.py
+++ plugin.video.monster/default.py
@@ -156,7 +156,7 @@ def scraper_settings():
 
 @route(mode="ResolverSettings")
 def resolver_settings():
-    xbmcaddon.Addon('script.module.urlresolver').openSettings()
+    xbmcaddon.Addon('script.module.resolverurl').openSettings()
 
 
 @route(mode="message", args=["url"])
